1dad49dffe07726d8327fae1162ae6968834f323
Tee Tanawatanakul <tee.tanawatanakul@capco.com>
2024-03-26 14:20:47 +0700

    Add chapeter HCL-01

---
+++ hcl-01/main.tf
@@ -0,0 +1,10 @@
+# resource "local_file" "sample_res" {
+#   filename = "sample.txt"
+#   # file_permission = "0755"
+#   content = "hello"
+# }
+
+# resource "local_sensitive_file" "sample_cred" {
+#   filename = "sample-cred.txt"
+#   content  = "Can you see me?"
+# }
