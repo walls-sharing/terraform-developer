0a40efaa2b7b6c5f77af1dece2ed713eeb02e78a
Tee Tanawatanakul <tee.tanawatanakul@capco.com>
2024-03-28 14:10:28 +0700

    Add chapter hcl-03

---
+++ hcl-03/outputs.tf
@@ -0,0 +1,22 @@
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
+
+output "pet_age" {
+  value = random_integer.pet_age.result
+}
+
+output "pet_password" {
+  sensitive = true
+  value = random_password.pet_password.result
+}
+
+
+output "pet_name_content" {
+  value = local_file.pet.content
+}
+
+output "pet_password_content" {
+  sensitive = true  # required
+  value = local_sensitive_file.pet_password.content
+}
