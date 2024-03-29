92d54c79e9b28f307150116e3ed1d08b65a38911
Tee Tanawatanakul <tee.tanawatanakul@capco.com>
2024-03-29 17:11:58 +0700

    Add chapter tg-01

---
+++ tg-01-gcloud-provider/main.tf
@@ -0,0 +1,35 @@
+# terraform {
+#   required_version = "~> 1.7.4"
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 5.20.0"
+#     }
+#     # random = {
+#     #     source = "hashicorp/random"
+#     #     version = "~> 3.5.0"
+#     # }
+#   }
+# }
+
+# provider "google" {
+#   project     = "focus-union-416313"  // ** change to your project name **
+#   region      = "asia-southeast1"
+#   zone        = "asia-southeast1-b"
+#   credentials = "../tf-gcloud.json"
+# }
+
+# resource "google_storage_bucket" "my_bucket" {
+#   location = "ASIA-SOUTHEAST1"
+#   name     = "terraform-mybucket"  // ** name need to be unique across the globe **
+# }
+
+# # # Utilize random provider
+# # resource "random_pet" "pet" {
+# #   separator = "-"
+# # }
+
+# # resource "google_storage_bucket" "pet_bucket" {
+# #   location = "ASIA-SOUTHEAST1"
+# #   name = "terraform-${random_pet.pet.id}"
+# # }
