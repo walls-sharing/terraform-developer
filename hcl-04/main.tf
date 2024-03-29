b82b63b5ae9d02fb0372225854be2d079e733fc1
Tee Tanawatanakul <tee.tanawatanakul@capco.com>
2024-03-29 13:21:06 +0700

    Comment codes in hcl-04

--- hcl-04/main.tf
+++ hcl-04/main.tf
@@ -1,10 +1,10 @@
-resource "local_file" "sample_file" {
-  filename = var.filename
-  content  = var.file_content
-}
+# resource "local_file" "sample_file" {
+#   filename = var.filename
+#   content  = var.file_content
+# }
 
-# https://developer.hashicorp.com/terraform/language/expressions/strings
-resource "local_file" "sample_file_sensitive" {
-  filename = "sensitive-${var.filename}"
-  content  = var.file_content_sensitive
-}
+# # https://developer.hashicorp.com/terraform/language/expressions/strings
+# resource "local_file" "sample_file_sensitive" {
+#   filename = "sensitive-${var.filename}"
+#   content  = var.file_content_sensitive
+# }
--- hcl-04/variables.tf
+++ hcl-04/variables.tf
@@ -1,18 +1,18 @@
-variable "filename" {
-  type        = string
-  description = "Sample filename"
-  default     = "sample.txt"
-}
+# variable "filename" {
+#   type        = string
+#   description = "Sample filename"
+#   default     = "sample.txt"
+# }
 
-variable "file_content" {
-  type        = string
-  description = "Sample file content"
-  default     = "Hello World!"
-}
+# variable "file_content" {
+#   type        = string
+#   description = "Sample file content"
+#   default     = "Hello World!"
+# }
 
-variable "file_content_sensitive" {
-  sensitive   = true
-  type        = string
-  description = "Sample sensitive file content"
-  default     = "Hello Secret!"
-}
+# variable "file_content_sensitive" {
+#   sensitive   = true
+#   type        = string
+#   description = "Sample sensitive file content"
+#   default     = "Hello Secret!"
+# }
