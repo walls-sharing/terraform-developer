bffceb446f6bc31d87a8435d652cbfe4f7a039fe
Tee Tanawatanakul <tee.tanawatanakul@capco.com>
2024-04-04 20:44:32 +0700

    Simplify chapter tg-01

--- practice/tg-01-gcloud-provider/course-material/main.tf
+++ practice/tg-01-gcloud-provider/course-material/main.tf
@@ -1,17 +1,18 @@
 terraform {
-  required_version = "~> 1.7.4"
+  required_version = "> 1.5.7"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.20.0"
     }
-    # random = {
-    #     source = "hashicorp/random"
-    #     version = "~> 3.5.0"
-    # }
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.5.0"
+    }
   }
 }
 
+# Config cloud project to use
 provider "google" {
   project     = "focus-union-416313"  // ** change to your project id **
   region      = "asia-southeast1"
@@ -19,17 +20,13 @@ provider "google" {
   credentials = "../tf-gcloud.json"
 }
 
+/*
+* Let's check provider config
+*/
+resource "random_pet" "pet" {
+  separator = "-"
+}
 resource "google_storage_bucket" "my_bucket" {
   location = "ASIA-SOUTHEAST1"
-  name     = "terraform-mybucket"  // ** need unique name across the globe **
+  name     = "terraform-${random_pet.pet.id}"  // ** need unique name across the globe **
 }
-
-# # Utilize random provider
-# resource "random_pet" "pet" {
-#   separator = "-"
-# }
-
-# resource "google_storage_bucket" "pet_bucket" {
-#   location = "ASIA-SOUTHEAST1"
-#   name = "terraform-${random_pet.pet.id}"
-# }
