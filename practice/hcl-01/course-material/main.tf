834b50adfcaeac063e611c000aa3782d13c13c6a
Tee Tanawatanakul <tee.tanawatanakul@capco.com>
2024-04-04 15:26:28 +0700

    Adjust scope of hcl-01

--- practice/hcl-01/course-material/main.tf
+++ practice/hcl-01/course-material/main.tf
@@ -3,8 +3,3 @@ resource "local_file" "sample_res" {
   # file_permission = "0755"
   content = "hello"
 }
-
-resource "local_sensitive_file" "sample_cred" {
-  filename = "sample-cred.txt"
-  content  = "Can you see me?"
-}
